2494bdf74c764c6eba9702ba82b793cc7f10b38f
sprThiru <87995087+sprThiru@users.noreply.github.com>
2021-07-28 10:27:38 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+Webcam.set({
+    height:300,
+    width:310,
+    img_format:'png',
+    png_quality:100,
+
+    constraints:
+    {
+        facingMode:"environment"
+    }
+});
+camera=document.getElementById("camera");
+Webcam.attach('#camera');
+
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri)
+    {
+        document.getElementById("result").innerHTML='<img id="captured_image" src="'+data_uri+'">';
+    }
+    )
+}
+
+console.log('ml5 version',ml5.version);
+classifier = ml5.imageClassifier('MobileNet',modelLoaded);
+
+function modelLoaded()
+    {
+        console.log('Model Loaded!');
+    }
+    function check()
+    {
+        img=document.getElementById('captured_img');
+        classifier.classify(img,gotResult);
+    }
+    function gotResult(error,results)
+    {
+        if (error)
+        {
+            console.error(error);
+        }
+        else{
+            console.log(results);
+            document.getElementById('object_name').innerHTML=results[0].label;
+        }
+    }
+
+
